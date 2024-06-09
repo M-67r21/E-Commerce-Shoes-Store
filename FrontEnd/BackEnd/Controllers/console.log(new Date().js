63275500137aedf5805f3a7cfba8ecde541b02ce
M-67r21 <Raphmutunga00@gmail.com>
2024-06-09 13:39:48 +0300

    Changed folders name

---
+++ FrontEnd/BackEnd/Controllers/console.log(new Date().js
@@ -0,0 +1,5 @@
+console.log(new Date().toISOString(0, 10)) 
+
+const end = new Date("2024-06-07T08:27:09.103Z")
+        end.setHours(23, 59, 59 ,999)
+        console.log(end)
--- FrontEnd/BackEnd/Routes/OrderRoutes.js
+++ FrontEnd/BackEnd/Routes/OrderRoutes.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const {verifyIsLoggedIn, verifyIsAdmin} = require("../CustomMiddlewares/VerifyAuthToken")
-const getUerOrders = require("../Controllers/OrderController")
+const {getUserOrders, getOrder, createOrder, updateOrderToPaid, updateOrderToDelivered, getOrders, getOrderForAnalysis} = require("../Controllers/OrderController")
 
 //user routes
 router.use(verifyIsLoggedIn)
-router.get("/", getUerOrders)
+router.get("/", getUserOrders)
+router.get("/user/:id", getOrder)
+router.post("/", createOrder)
+router.put("/paid/:id" , updateOrderToPaid)
 
 //admin routes
 router.use(verifyIsAdmin)
+router.put("/delivered/:id", updateOrderToDelivered)
+router.get("/admin", getOrders)
+router.get("/analysis/:date", getOrderForAnalysis)
 
 module.exports = router
